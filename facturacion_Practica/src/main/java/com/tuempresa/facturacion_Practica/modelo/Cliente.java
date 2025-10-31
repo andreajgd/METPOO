0415bfdd85ab8e9d4e4bcd2d04ba9309979b1ab7
Andrea Duarte <ajduarteg@uamv.edu.ni>
2025-10-30 18:26:12 -0600

    Add Categoria

--- facturacion_Practica/src/main/java/com/tuempresa/facturacion_Practica/modelo/Cliente.java
+++ facturacion_Practica/src/main/java/com/tuempresa/facturacion_Practica/modelo/Cliente.java
@@ -1,22 +1,24 @@
 package com.tuempresa.facturacion_Practica.modelo;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.openxava.annotations.Required;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.openxava.annotations.*;
+
+import lombok.*;
 
 @Entity  // Esto marca la clase Cliente como una entidad
-@Getter
-@Setter // Esto hace los campos a continuación públicamente accesibles
+@Getter @Setter // Esto hace los campos a continuaci?n p?blicamente accesibles
 public class Cliente {
+
     @Id  // La propiedad numero es la clave.  Las claves son obligatorias (required) por defecto
     @Column(length=6)  // La longitud de columna se usa a nivel UI y a nivel DB
     int numero;
 
     @Column(length=50) // La longitud de columna se usa a nivel UI y a nivel DB
-    @Required  // Se mostrará un error de validación si la propiedad nombre se deja en blanco
+    @Required  // Se mostrar? un error de validaci?n si la propiedad nombre se deja en blanco
     String nombre;
+
+    @Embedded // As? para referenciar a una clase incrustable
+    Direccion direccion; // Una referencia Java convencional
+
 }
--- facturacion_Practica/src/main/java/com/tuempresa/facturacion_Practica/modelo/Producto.java
+++ facturacion_Practica/src/main/java/com/tuempresa/facturacion_Practica/modelo/Producto.java
@@ -1,20 +1,35 @@
 package com.tuempresa.facturacion_Practica.modelo;
+import java.math.*;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.openxava.annotations.Required;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.openxava.annotations.*;
 
-@Entity
-@Getter @Setter
+import lombok.*;
+
+@Entity @Getter @Setter
 public class Producto {
-    @Id
-    @Column(length=9)
+
+    @Id @Column(length=9)
     int numero;
 
     @Column(length=50) @Required
     String descripcion;
+
+    @ManyToOne( // La referencia se almacena como una relación en la base de datos
+            fetch=FetchType.LAZY, // La referencia se carga bajo demanda
+            optional=true) // La referencia puede estar sin valor
+    @DescriptionsList // Así la referencia se visualiza usando un combo
+    Categoria categoria; // Una referencia Java convencional
+
+    @Money // La propiedad precio se usa para almacenar dinero
+    BigDecimal precio; // BigDecimal se suele usar para dinero
+
+    @Files // Una galería de fotos completa está disponible
+    @Column(length=32) // La cadena de 32 de longitud es para almacenar la clave de la galería
+    String fotos;
+
+    @TextArea // Esto es para un texto grande, se usará un área de texto o equivalente
+    String observaciones;
+
 }
